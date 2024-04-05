e0220d77b24776d26b535da0e7559e7cedbeaf6b
llouliLafuente <141773015+llouliLafuente@users.noreply.github.com>
2024-04-05 12:46:33 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,13 +1,12 @@
 def slice_simple():
-    texto = "Awesome"
+    txt = "Awesome"
+    print(txt[0:3].lower())
+    print(txt[2:5])
+    print(txt[0:4].lower() + txt[4:7].lower())
+    
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
-def slice_simple():
-    texto = input("ingrese un texto: ")
-    print(texto[4::2])
-    
-slice_simple()
